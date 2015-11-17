0cc97dd618f3e1c70f94098acc0f6838aaeabb5f
Teodor Todorov <teodor.todorov@cgm.com>
2015-11-17 09:48:00 +0200

    changed the index name and position everything works so far

--- AngularAPP/app/module/app.js
+++ AngularAPP/app/module/app.js
@@ -6,12 +6,12 @@ angular.module('app', ['ui.router', 'restangular', 'ngMessages']).
         RestangularProvider.setBaseUrl('http://localhost:1337/');
 
         // For any unmatched url, send to /home/index
-        $urlRouterProvider.otherwise("/home/index");
+        //$urlRouterProvider.otherwise("/home/index");
 
         $stateProvider
             .state('home', {
                 url: "/home",
-                templateUrl: "../views/home.html",
+                templateUrl: "views/home.html",
                 abstract: true
             })
           .state('home.index', {
@@ -20,14 +20,14 @@ angular.module('app', ['ui.router', 'restangular', 'ngMessages']).
           })
             .state('home.purchasesByDay', {
                 url: "/purchases-by-day/:dayId",
-                templateUrl: "../views/purchasesByDay.html",
+                templateUrl: "views/purchasesByDay.html",
                 controller: 'purchaseController'
             })
             .state('home.purchasesByDay.addPurchase', {
                 url: "/add-purchase",
                 views: {
                   "add": {
-                        templateUrl: "../views/addPurchase.html"
+                        templateUrl: "views/addPurchase.html"
                   }
                 }
             });
