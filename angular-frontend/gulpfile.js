5c96a2ecccb23374fec6ce7a6aec6e55ca5cff5d
Teodor Todorov <teodor.todorov@cgm.com>
2015-11-18 16:19:49 +0200

    finished with changing the HTML and the gulp file

--- angular-frontend/gulpfile.js
+++ angular-frontend/gulpfile.js
@@ -5,7 +5,7 @@
 
 // Load plugins
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
@@ -45,13 +45,17 @@ gulp.task('run-app', function(){
 
 // Styles
 gulp.task('styles', function() {
-  return sass('app/styles/main-style.scss', { style: 'expanded' })
+    gulp.src('app/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(notify({ message: 'Styles task complete' }));
+  /*return sass('app/styles/main-style.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('dist/styles'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/styles'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(notify({ message: 'Styles task complete' }));*/
 });
 
 // Scripts
